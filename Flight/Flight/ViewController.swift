e2cd500b0b2145ed1f9225ea22ebbf49299dbdd6
bhupesh <bhupesh@ixigo.com>
2021-11-23 15:46:28 +0530

    Added flight and train app

---
+++ Flight/Flight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flight
+//
+//  Created by bhupesh on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Train/Train/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Train
+//
+//  Created by bhupesh on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
